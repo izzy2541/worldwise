298394de88360a2993680d50f0d8baf8137fc6e0
izzy2541 <isabel.donnelly@hotmail.com>
2025-09-24 13:06:18 +1000

    Nested Routes and index route

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,22 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
-function App(){
+function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />}/>
-      <Route path="/product" element={<Product />} />
-      <Route path="/pricing" element={<Pricing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="app" element={<AppLayout />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>}/>
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </BrowserRouter>
   )
 }
