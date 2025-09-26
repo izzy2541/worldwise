b3941c55f6aebb86dca58c88421dadc1a1dd6f82
izzy2541 <isabel.donnelly@hotmail.com>
2025-09-26 19:11:34 +1000

    Reading and setting a query string

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,8 +1,11 @@
-import { useParams } from "react-router-dom";
+// import { useParams, useSearchParams } from "react-router-dom";
 // import styles from "./City.module.css";
 
 function City() {
-  const { id } = useParams();
+  // const { id } = useParams();
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const lat = searchParams.get("lat");
+  // const lng = searchParams.get("lng");
 
   // TEMP DATA
   // const currentCity = {
@@ -14,7 +17,13 @@ function City() {
 
   // const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>
+  return (
+    <>
+      {/* <h1>City {id}</h1> */}
+      <h1>City</h1>
+      {/* <p>Position: {lat}, {lng}</p> */}
+    </>
+  )
 
   // return (
   //   <div className={styles.city}>
