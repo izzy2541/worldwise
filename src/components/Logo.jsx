212dac518dfc3257aa31175e3b00a02c9e63fbc2
izzy2541 <isabel.donnelly@hotmail.com>
2025-10-13 19:31:45 +1100

    mobile width adjustments

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,11 +2,15 @@ import styles from "./Logo.module.css";
 import { Link } from "react-router-dom";
 
 function Logo() {
-    return (
-        <Link to="/">
-            <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
-        </Link>
-    )
+  return (
+    <Link to="/">
+      <picture>
+        {/* Show icon.png when screen ≤ 600px */}
+        <source srcSet="/icon.png" media="(max-width: 510px)" />
+        <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
+      </picture>
+    </Link>
+  );
 }
 
 export default Logo;
