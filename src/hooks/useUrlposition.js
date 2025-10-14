293aecc7fdc50b3e799b9c7ee3e1acfd5fdbc820
izzy2541 <isabel.donnelly@hotmail.com>
2025-10-14 19:45:49 +1100

    update useurlposition to correct file name

--- src/hooks/useUrlposition.js
+++ src/hooks/useUrlposition.js
@@ -1,7 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 
 export function useUrlPosition() {
-    //returns an array that contains the current state
+    //returns an array that contains the current stat
     const [searchParams] = useSearchParams();
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
